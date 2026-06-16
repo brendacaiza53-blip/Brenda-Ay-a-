9c4091755a5a35c3bb80f5de29c8cf50ec03aab6
Brenda Caiza <brendacaiza53@gmail.com>
2026-06-16 12:12:08 -0500

    Primer commit del proyecto

---
+++ mail.py
@@ -0,0 +1,15 @@
+
+# ==========================================
+# Proyecto de Programación I
+# Autora: Brenda Caiza
+# ==========================================
+
+print("===================================")
+print("      PROYECTO DE PROGRAMACIÓN")
+print("===================================")
+
+print("Nombre: Brenda Caiza")
+print("Carrera: Ingeniería en Software")
+print("Materia: Programación I")
+
+print("\n¡Bienvenido al proyecto!")
